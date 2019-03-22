86b0c6fc829c377df68a1d25c3c89a14bbc8f0d4
Warren Whipple <w@warrenwhipple.com>
2019-03-22 16:36:26 -0400

    Add defaults

--- dev/dev.js
+++ dev/dev.js
@@ -2,7 +2,7 @@ $(function() {
   // Generate random item data
   for (var i = 0; i < 50; i++)
     $('.grid').append(
-      '<div class="item"><img src="' +
+      '<div class="grid-item"><img src="' +
         faker.image.avatar() +
         '"><h2 class="name">' +
         faker.name.findName() +
@@ -14,12 +14,5 @@ $(function() {
     );
 
   // Initialize isotope search
-  isotopeSearch.init({
-    stickySelector: 'input',
-    stickyOffset: 16,
-    gridSelector: '.grid',
-    gridItemSelector: '.item',
-    columnSizerSelector: '.column-sizer',
-    gutterSizerSelector: '.gutter-sizer',
-  });
+  isotopeSearch.init({});
 });
--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import stickybits from 'stickybits';
 import Isotope from 'isotope-layout';
 import imagesLoaded from 'imagesloaded';
 
-export function init({
-  stickySelector,
-  stickyOffset,
-  gridSelector,
-  gridItemSelector,
-  columnSizerSelector,
-  gutterSizerSelector,
-}) {
+export function init(options = {}) {
+  const defaultOptions = {
+    stickySelector: '.sticky',
+    stickyOffset: 16,
+    gridSelector: '.grid',
+    gridItemSelector: '.grid-item',
+    columnSizerSelector: '.column-sizer',
+    gutterSizerSelector: '.gutter-sizer',
+  };
+  const {
+    stickySelector,
+    stickyOffset,
+    gridSelector,
+    gridItemSelector,
+    columnSizerSelector,
+    gutterSizerSelector,
+  } = Object.assign({}, defaultOptions, options);
+
   // Sticky search bar
   stickybits(stickySelector, { stickyBitStickyOffset: stickyOffset });
 
@@ -26,7 +36,7 @@ export function init({
     },
   });
 
-  // Trigger layout after images load
+  // Refresh layout after images load
   imagesLoaded(gridSelector, () => {
     iso.layout();
   });
